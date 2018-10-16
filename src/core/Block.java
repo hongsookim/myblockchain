2d35a0691c464d2125e389670151a4a1c410cfdc
Kim Hongsoo <hongsoo.kim@brilliantts.com>
2018-10-16 17:31:55 +0900

    modified release 2

--- src/core/Block.java
+++ src/core/Block.java
@@ -2,17 +2,11 @@ package core;
 
 import util.*;
 
-import java.util.ArrayList;
-import java.math.BigInteger;
-import java.security.Signature;
-
 public class Block {
-	private static final String ALGORITHM = "SHA1withECDSA";
-	
 	private int blockID;
 	private String previousBlockHash;
 	private int nonce;
-	private ArrayList<Transaction> transactionList;
+	private String data;
 
 	public int getBlockID() {
 	    return blockID;
@@ -37,62 +31,43 @@ public class Block {
 	public void setNonce(int nonce) {
 	    this.nonce = nonce;
 	}
+	
+	public String getData() {
+		return data;
+	}
+	  
+	public void setData(String data) {
+		this.data = data;
+	}
 
-	public Block(int blockID, String previousBlockHash, int nonce, ArrayList<Transaction> transactionList) {
+	public Block(int blockID, String previousBlockHash, int nonce, String data) {
 		this.blockID = blockID;
 		this.previousBlockHash = previousBlockHash;
 		this.nonce = nonce;
-		this.transactionList = transactionList;
+		this.data = data;
 	}
 	
-	private boolean verifyTransaction(Transaction transaction) throws Exception {
-		Signature signature;
-		signature = Signature.getInstance(ALGORITHM);
-		byte[] baText = transaction.getData().getBytes("UTF-8");
-		signature.initVerify(transaction.getSender());
-		signature.update(baText);
-		return signature.verify(new BigInteger(transaction.getSignature(), 16).toByteArray());
+	public void getInformation() {
+		System.out.println("--------------------------------------");
+		System.out.println("Block Number: " + getBlockID());
+		System.out.println("Previous Block Hash: " + getPreviousBlockHash());
+		System.out.println("Nonce: " + getNonce());
+		System.out.println("Block Data: " + getData());
+		System.out.println("Block Hash: " + getBlockHash());
+		System.out.println("--------------------------------------");
 	}
-	
-	public void addTransaction(Transaction transaction) throws Exception {
-		if(verifyTransaction(transaction)) {
-			System.out.println("Good Transaction!");
-			transactionList.add(transaction);
-		} else {
-			System.out.println("Bad Transaction!");			
+
+  	public String getBlockHash() {
+  		return Util.getHash(nonce + data + previousBlockHash);	  
+  	}
+  
+	public void mine() {
+		while(true) {
+			if(getBlockHash().substring(0, 4).equals("0000")) {
+				System.out.println("Success "+blockID+"th block mining.");
+				break;
+			}
+			nonce++;
 		}
-		
 	}
-
-	  public void showInformation() {
-		  System.out.println("--------------------------------------");
-		  System.out.println("Block Number: " + getBlockID());
-		  System.out.println("Previous Block Hash: " + getPreviousBlockHash());
-		  System.out.println("Nonce: " + getNonce());
-		  System.out.println("Transaction Number: " + transactionList.size());
-		  for(int i = 0; i < transactionList.size(); i++) {
-			  System.out.println(transactionList.get(i).getInformation());
-		  }
-		  System.out.println("Block Hash: " + getBlockHash());
-		  System.out.println("--------------------------------------");
-	  }
-
-	  public String getBlockHash() {
-		  String transactionInformations = "";
-		  for(int i = 0; i < transactionList.size(); i++) {
-		  	transactionInformations += transactionList.get(i).getInformation();
-		  }
-
-		  return Util.getHash(nonce + transactionInformations + previousBlockHash);	  
-	  }
-	  
-	  public void mine() {
-		  while(true) {
-			  if(getBlockHash().substring(0, 4).equals("0000")) {
-				  System.out.println("Success "+blockID+"th block mining.");
-				  break;
-			  }
-			  nonce++;
-		  }
-	  }
 }
--- src/core/BlockChainStarter.java
+++ src/core/BlockChainStarter.java
@@ -1,58 +1,21 @@
 package core;
 
-import java.math.BigInteger;
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.security.Security;
-import java.security.Signature;
-import java.util.ArrayList;
-
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
-
-import util.*;
-
 public class BlockChainStarter {
 	  public static void main(String[] args) throws Exception{
-		  
-		    Security.addProvider(new BouncyCastleProvider());
-	
-		    Wallet wallet1 = new Wallet();
-		    wallet1.setFromFile("private1.pem", "public1.pem");
-		    Wallet wallet2 = new Wallet();
-		    wallet2.setFromFile("private2.pem", "public2.pem");
-		    Wallet wallet3 = new Wallet();
-		    wallet3.setFromFile("private3.pem", "public3.pem");
-		    
-		    //blockID, previousBlockHash, nonce, transactionList
-		    Block block1 = new Block( 1, null, 0, new ArrayList<Transaction>() );
+		    Block block1 = new Block(1, null, 0, "data");
 		    block1.mine();
-		    block1.showInformation();
+		    block1.getInformation();
 		    
-		    Block block2 = new Block(2, block1.getBlockHash(), 0, new ArrayList<Transaction>());
-		    
-		    //Create transaction to send from wallet1 to wallet2
-		    Transaction transaction1 = new Transaction(wallet1, wallet2.getPublicKey(), 1.5, "2018-05-03 23:05:19.5");
-		    block2.addTransaction(transaction1);
-		    
-		    //Create transaction to send from wallet2 to wallet3
-		    Transaction transaction2 = new Transaction(wallet2, wallet3.getPublicKey(), 3.7, "2018-05-04 14:12:09.5");
-		    block2.addTransaction(transaction2);
-		    
+		    Block block2 = new Block(2, block1.getBlockHash(), 0, "data");	//"modified data"
 		    block2.mine();
-		    block2.showInformation();
+		    block2.getInformation();
 		    
-		    Block block3 = new Block(3, block2.getBlockHash(), 0, new ArrayList<Transaction>());
-
-		    //Create transaction to send from wallet1 to wallet3
-		    Transaction transaction3 = new Transaction(wallet1, wallet3.getPublicKey(), 2.3, "2018-05-06 17:09:21.5");
-		    block3.addTransaction(transaction3);
-		    
-		    //Create transaction to send from wallet2 to wallet3
-		    Transaction transaction4 = new Transaction(wallet2, wallet3.getPublicKey(), 1.4, "2018-05-07 02:11:19.5");
-		    block3.addTransaction(transaction4);
-		    
+		    Block block3 = new Block(3, block2.getBlockHash(), 0, "data");
 		    block3.mine();
-		    block3.showInformation();
-		  
+		    block3.getInformation();
+
+		    Block block4 = new Block(4, block3.getBlockHash(), 0, "data");
+		    block4.mine();
+		    block4.getInformation();
 	  }
 }
