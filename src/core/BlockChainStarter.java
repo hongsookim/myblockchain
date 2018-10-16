2ba3a25430d8d495aa3d2b7ce893d8617b5d5dc0
Kim Hongsoo <hongsoo.kim@brilliantts.com>
2018-10-16 17:22:14 +0900

    modified hashing

--- src/core/BlockChainStarter.java
+++ src/core/BlockChainStarter.java
@@ -1,18 +1,16 @@
 package core;
 
-import java.math.BigInteger;
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.security.Security;
-import java.security.Signature;
-import java.util.ArrayList;
-
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
-
 import util.*;
 
 public class BlockChainStarter {
 	  public static void main(String[] args) throws Exception{
-		  System.out.println(Util.getHash("0"));		  
+		    int nonce = 0;
+		    while(true) {
+			    if(Util.getHash(nonce + "").substring(0, 6).equals("000000")) {
+			        System.out.println("Answer: " + nonce);
+			        break;
+			    }
+			    nonce++;
+		    }	  
 	  }
 }
