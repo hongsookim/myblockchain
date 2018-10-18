f382a2d1c1a1571920b4948fafc0917ef88d8ee5
Kim Hongsoo <hongsoo.kim@brilliantts.com>
2018-10-18 16:16:06 +0900

    modified mining level

--- src/core/BlockChainStarter.java
+++ src/core/BlockChainStarter.java
@@ -6,6 +6,7 @@ public class BlockChainStarter {
 	  public static void main(String[] args) throws Exception{
 		    int nonce = 0;
 		    while(true) {
+		    	System.out.println("Hashed Value: " + Util.getHash(nonce + ""));
 			    if(Util.getHash(nonce + "").substring(0, 6).equals("000000")) {
 			        System.out.println("Answer: " + nonce);
 			        break;
