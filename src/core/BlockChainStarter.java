c339e6f9d90c49c599dadc42c290329e2f7df0e2
hongsookim <floodkim@gmail.com>
2018-10-26 14:12:15 +0900

    Update BlockChainStarter.java

--- src/core/BlockChainStarter.java
+++ src/core/BlockChainStarter.java
@@ -7,6 +7,7 @@ public class BlockChainStarter {
 		    int nonce = 0;
 		    while(true) {
 		    	System.out.println("Hashed Value: " + Util.getHash(nonce + ""));
+			    //Difficuly? It's up and down due to the number of zero
 			    if(Util.getHash(nonce + "").substring(0, 6).equals("000000")) {
 			        System.out.println("Answer: " + nonce);
 			        break;
