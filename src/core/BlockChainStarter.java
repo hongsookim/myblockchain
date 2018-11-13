53c554278ebaa04eb218ac2b18932baae67ffd02
Kim Hongsoo <hongsoo.kim@brilliantts.com>
2018-11-13 16:54:41 +0900

    modified something

--- src/core/BlockChainStarter.java
+++ src/core/BlockChainStarter.java
@@ -5,6 +5,7 @@ import util.*;
 public class BlockChainStarter {
 	  public static void main(String[] args) throws Exception{
 		    int nonce = 0;
+		    
 		    while(true) {
 		    	System.out.println("Hashed Value: " + Util.getHash(nonce + ""));
 			    if(Util.getHash(nonce + "").substring(0, 6).equals("000000")) {
